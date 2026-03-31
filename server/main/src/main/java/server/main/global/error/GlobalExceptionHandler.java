4069864bb520ee721ac64dbec66a93cf8f2f1045
nippyclouding <wonderwall09in@naver.com>
2026-03-31 12:03:36 +0900

    error fix

--- server/main/src/main/java/server/main/global/error/GlobalExceptionHandler.java
+++ server/main/src/main/java/server/main/global/error/GlobalExceptionHandler.java
@@ -14,9 +14,6 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /**
-     * javax.validation.Valid 또는 @Validated binding error가 발생할 경우
-     */
     @ExceptionHandler(BindException.class)
     protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
         log.error("handleBindException", e);
@@ -26,9 +23,6 @@ public class GlobalExceptionHandler {
                 .body(errorResponse);
     }
 
-    /**
-     * 주로 @RequestParam enum으로 binding 못했을 경우 발생
-     */
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     protected ResponseEntity<ErrorResponse>
     handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
@@ -39,9 +33,6 @@ public class GlobalExceptionHandler {
                 .body(errorResponse);
     }
 
-    /**
-     * 지원하지 않은 HTTP method 호출 할 경우 발생
-     */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<ErrorResponse>
     handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
@@ -51,9 +42,6 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
     }
 
-    /**
-     * 비즈니스 로직 실행 중 오류 발생
-     */
     @ExceptionHandler(value = {BusinessException.class})
     protected ResponseEntity<ErrorResponse> handleConflict(BusinessException e) {
         log.error("BusinessException", e);
@@ -62,9 +50,6 @@ public class GlobalExceptionHandler {
                 .body(errorResponse);
     }
 
-    /**
-     * 나머지 예외 발생
-     */
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error("Exception", e);
