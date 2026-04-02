cd42d9319e0ea3d3fe6ca540ff5d3ec541af0407
BGCHOI <chlqjarms1@gmail.com>
2026-04-02 17:16:21 +0900

    ì자산 등록 시 í첨부파일 업로드

---
+++ server/main/src/main/java/server/main/diclosure/entity/Disclosure.java
@@ -0,0 +1,26 @@
+package server.main.diclosure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import server.main.asset.entity.Asset;
+import server.main.global.util.BaseEntity;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Disclosure extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long disclosureId;      // 공시ID
+    private String disclosureTitle;     // 공시 제목
+    @Column(name = "disclosureContent", columnDefinition = "TEXT")
+    private String disclosureContent;   // 공시본문
+    @Enumerated(EnumType.STRING)  // ← STRING으로 변경
+    @Column(name = "disclosure_category")
+    private DisclosureCategory disclosureCategory;  // 공시 타입
+
+    private Long assetId;       // 부동산ID
+}
---
+++ server/main/src/main/java/server/main/diclosure/entity/DisclosureCategory.java
@@ -0,0 +1,5 @@
+package server.main.diclosure.entity;
+
+public enum DisclosureCategory {
+    BUILDING, DIVIDEND, ETC
+}
