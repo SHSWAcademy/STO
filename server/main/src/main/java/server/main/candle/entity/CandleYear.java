4ff4676022c2727d1b4e4609bba51cac08ba6225
nippyclouding <wonderwall09in@naver.com>
2026-04-08 14:25:39 +0900

    캔들차트 실시간 작업중

--- server/main/src/main/java/server/main/candle/entity/CandleYear.java
+++ server/main/src/main/java/server/main/candle/entity/CandleYear.java
@@ -3,11 +3,13 @@ package server.main.candle.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @Table(name = "CANDLE_YEARS")
 @NoArgsConstructor
+@SuperBuilder
 public class CandleYear extends Candle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
