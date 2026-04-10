d2c6314b753088d21ee6c24f114a0bb5fcde2a8b
nippyclouding <wonderwall09in@naver.com>
2026-04-10 10:56:07 +0900

    복제 테이블

--- server/main/src/main/java/server/main/order/entity/OrderDuplicated.java
+++ server/main/src/main/java/server/main/order/entity/OrderDuplicated.java
@@ -33,5 +33,10 @@ public class OrderDuplicated {
     private Long orderSequence;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    private LocalDateTime archivedAt;
+    private LocalDateTime archivedAt; // 주문이 최종 종료되어 duplicated로 들어온 시점
+//    createdAt (10:00): 고객이 물건을 주문한 시간
+//
+//    updatedAt (14:00): 택배 기사님이 배송을 시작한 시간 (상태 변경)
+//
+//    archivedAt (16:00): 배송이 완전히 끝나서 '완료' 목록으로 넘어간 시간
 }
