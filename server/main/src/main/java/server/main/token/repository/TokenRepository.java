a2927bec6f94c6199bc2c29ae9473207990504f8
hot-gamja <coke0701@gmail.com>
2026-04-20 12:37:26 +0900

    fix: 일봉 슬롯 ts 불일치로 캔들 미표시 버그 수정
      getBucketStart('일') 기준을 00:00에서 09:00으로 수정.
      오전 9시 이전 접속 시 전날 09:00을 현재 슬롯으로 인식하도록 처리.

--- server/main/src/main/java/server/main/token/repository/TokenRepository.java
+++ server/main/src/main/java/server/main/token/repository/TokenRepository.java
@@ -1,13 +1,14 @@
 package server.main.token.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
 import server.main.token.entity.Token;
 import server.main.token.entity.TokenStatus;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> , TokenRepositoryCustom {
 
